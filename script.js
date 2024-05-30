7efbd3e3affedae9f76c2a674da9e0106707327d
ABuffaloHerd <blaster140902@gmail.com>
2024-05-30 15:38:57 +1200

    it shows text now but no ingredients

--- script.js
+++ script.js
@@ -2,5 +2,19 @@ document.addEventListener('DOMContentLoaded', () =>
 {
     fetch('file.json')
         .then(response => response.json())
-        .then(data => console.log(data))
+        .then(data =>
+            {
+                const treeContainer = document.getElementById('tree-container');
+                renderTree(data, treeContainer)
+                console.log(data)
+            }
+        )
 })
+
+function renderTree(data, container) {
+    data["chems"].forEach(element => {
+        const thing = document.createElement('div')
+        thing.textContent = element["name"]
+        container.appendChild(thing)    
+    });
+}
